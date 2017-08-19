0d8ad52f77cc135c7bb78351bbb22dbeb4e31ae8
CaiquanLiu <379953356@qq.com>
2017-08-19 16:14:59 +0800

    添加微信公众号后台服务器配置支持

--- src/main.py
+++ src/main.py
@@ -3,7 +3,11 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        echo_str = self.get_argument('echostr', default='_ARG_DEFAULT')
+        if echo_str.strip()=='_ARG_DEFAULT':
+            self.write("this is MyWeChatService!")
+        else:
+            self.write(echo_str)
 
 def make_app():
     return tornado.web.Application([
@@ -12,5 +16,5 @@ def make_app():
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(80)
+    app.listen(801)
     tornado.ioloop.IOLoop.current().start()

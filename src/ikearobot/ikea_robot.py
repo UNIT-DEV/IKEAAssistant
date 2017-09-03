7cb7bdcde411d5426e9b16338923d6e68341c64a
eddy <379953356@qq.com>
2017-09-04 00:13:23 +0800

    将database添加到主题框架中

--- src/ikearobot/ikea_robot.py
+++ src/ikearobot/ikea_robot.py
@@ -8,6 +8,7 @@ Created on 2017年8月30日
 from baiduunit.unit import BaiduUnit
 from intent import Intent
 import json
+from database.ikea_database import IkeaDatabase
 
 
 class IkeaRobot(object):
@@ -15,6 +16,7 @@ class IkeaRobot(object):
         self.unit = BaiduUnit()
         self.scene_id = 9633
         self.min_confidence = 50
+        self.database = IkeaDatabase()
 
     def request(self, query):
         unit_rst = self.unit.query_request(self.scene_id, query, "").text
@@ -22,7 +24,7 @@ class IkeaRobot(object):
         if (bot_intent.get_intent_confidence() < self.min_confidence):
             return ''
         else:
-            # TODO:根据实际的intent和slot搜索出合适的结果
+            # TODO:根据实际的intent和slot搜索出合适的结果(数据来源self.database)
             dic = {}
             dic['intent'] = bot_intent.get_intent()
             dic['confidence'] = bot_intent.get_intent_confidence()

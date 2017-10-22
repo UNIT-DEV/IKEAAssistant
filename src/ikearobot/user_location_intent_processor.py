d70e7c77ae1560eddece691987def524a3cb98fc
eddy <379953356@qq.com>
2017-10-22 23:19:02 +0800

    修改代码重构导致的位置信息查询失败问题

--- src/ikearobot/user_location_intent_processor.py
+++ src/ikearobot/user_location_intent_processor.py
@@ -29,9 +29,9 @@ class UserLocationProcessor(object):
                 返回值：完整的url地址字符串
         '''
         rst = ''
-        rst += global_common_params.web_server_url
-        rst += '?' + request_params.key_req_get_type + '=' + request_params.val_req_get_type_webpage
-        rst += '&' + request_params.key_req_get_html_file_name + '=' + html_file_name
+        rst += global_common_params.WEB_SERVER_URL
+        rst += '?' + request_params.KEY_REQ_GET_TYPE + '=' + request_params.VAL_REQ_GET_TYPE_WEBPAGE
+        rst += '&' + request_params.KEY_REQ_GET_HTML_FILE_NAME + '=' + html_file_name
 
         return rst
 
@@ -42,29 +42,29 @@ class UserLocationProcessor(object):
                 返回值：结果字典
         '''
         rsp_dict = {}
-        rsp_dict[wechat_msg_params.key_message_type] = wechat_msg_params.val_msg_type_invalid
+        rsp_dict[wechat_msg_params.KEY_MESSAGE_TYPE] = wechat_msg_params.VAL_MSG_TYPE_INVALID
 
         location = intent.get_slot_location()
 
         # slot中没有位置信息
         if location is None:
-            rsp_dict[wechat_msg_params.key_message_type] = wechat_msg_params.val_msg_type_text
-            rsp_dict[wechat_msg_params.key_content] = u'抱歉，没有找到您需要查找的位置~'
+            rsp_dict[wechat_msg_params.KEY_MESSAGE_TYPE] = wechat_msg_params.VAL_MSG_TYPE_TEXT
+            rsp_dict[wechat_msg_params.KEY_CONTENT] = u'抱歉，没有找到您需要查找的位置~'
             return rsp_dict
 
         index, description = self.database.find_location(location)
 
         # 位置信息无效
         if (index <= 0):
-            rsp_dict[wechat_msg_params.key_message_type] = wechat_msg_params.val_msg_type_text
-            rsp_dict[wechat_msg_params.key_content] = u'抱歉，没有找到您需要查找的位置~'
+            rsp_dict[wechat_msg_params.KEY_MESSAGE_TYPE] = wechat_msg_params.VAL_MSG_TYPE_TEXT
+            rsp_dict[wechat_msg_params.KEY_CONTENT] = u'抱歉，没有找到您需要查找的位置~'
             return rsp_dict
 
-        html_file_name = self.html_builder.location_build(ikearobot_params.pic_resource_dict[index], description)
+        html_file_name = self.html_builder.location_build(ikearobot_params.PIC_RESOURCE_DICT[index], description)
 
-        rsp_dict[wechat_msg_params.key_message_type] = wechat_msg_params.val_msg_type_news
-        rsp_dict[wechat_msg_params.key_msg_content_title] = location + u'位置信息'
-        rsp_dict[wechat_msg_params.key_msg_content_description] = u'点击查看详细的位置信息'
-        rsp_dict[wechat_msg_params.key_msg_content_pciurl] = ikearobot_params.pic_resource_dict[index]
-        rsp_dict[wechat_msg_params.key_msg_content_url] = self.__build_webpage_get_url(html_file_name)
+        rsp_dict[wechat_msg_params.KEY_MESSAGE_TYPE] = wechat_msg_params.VAL_MSG_TYPE_NEWS
+        rsp_dict[wechat_msg_params.KEY_MSG_CONTENT_TITLE] = location + u'位置信息'
+        rsp_dict[wechat_msg_params.KEY_MSG_CONTENT_DESCRIPTION] = u'点击查看详细的位置信息'
+        rsp_dict[wechat_msg_params.KEY_MSG_CONTENT_PIC_URL] = ikearobot_params.PIC_RESOURCE_DICT[index]
+        rsp_dict[wechat_msg_params.KEY_MSG_CONTENT_URL] = self.__build_webpage_get_url(html_file_name)
         return rsp_dict
